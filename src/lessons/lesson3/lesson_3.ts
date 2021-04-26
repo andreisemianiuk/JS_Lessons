afb40fc26ff101650c4aed652c55e1e47f2871ad
MrBo88 <Mr@Bo88.io>
2021-04-26 09:27:21 +0300

    did all tasks

--- src/lessons/lesson3/lesson_3.ts
+++ src/lessons/lesson3/lesson_3.ts
@@ -1,4 +1,6 @@
-console.log('lesson 3');
+import axios from 'axios'
+
+console.log('lesson 3')
 
 // Event loop
 // https://learn.javascript.ru/event-loop
@@ -11,10 +13,26 @@ console.log('lesson 3');
 // https://learn.javascript.ru/promise-basics
 // https://www.youtube.com/watch?v=1idOY3C1gYU
 
-
 // https://jsonplaceholder.typicode.com/posts/1
 // https://habr.com/ru/company/oleg-bunin/blog/417461/?_ga=2.54695343.543933152.1602500664-1040035071.1596811661
 
+const instance = axios.create({
+  baseURL: 'https://jsonplaceholder.typicode.com',
+})
+
+// instance.get(`posts`).then(res => console.log(res.data))
+
+// instance.post('posts', { userId: 1000, title: 'hey', body: 'b' }).then(
+//   res => console.log(res.data),
+//   err => console.log(err)
+// )
+
+// instance.put(`posts/100`, { userId: 100, title: 'hey', body: 'b' }).then(
+//   res => console.log(res.data),
+//   err => console.log(err)
+// )
+
+// instance.delete(`posts/2`).then(res => console.log(res))
 
 // just a plug
-export default ()=>{};
+export default () => {}
