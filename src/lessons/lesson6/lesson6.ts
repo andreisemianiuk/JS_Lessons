c0bbfa4b88096840da0d0017ea8b6131d4a43a84
MrBo88 <Mr@Bo88.io>
2021-05-17 21:29:17 +0300

    lesson 6 / task 2

--- src/lessons/lesson6/lesson6.ts
+++ src/lessons/lesson6/lesson6.ts
@@ -9,92 +9,106 @@ console.log('Lesson 6')
 
 // Task 01
 // Создайте структуру с именем student, содержащую поля: имя и фамилия, номер группы, успеваемость (массив из пяти элементов).
-
-interface IStudent {
-  name: string
-  lastName: string
-  group: number
-  academicPerformance: number[]
-  averageMark: number
-}
-
-class Student implements IStudent {
-  name: string
-  lastName: string
-  group: number
-  academicPerformance: number[]
-  averageMark: number
-
-  constructor(
-    name: string,
-    lastName: string,
-    group: number,
-    academicPerformance: number[]
-  ) {
-    this.name = name
-    this.lastName = lastName
-    this.group = group
-    this.academicPerformance = academicPerformance
-    this.averageMark = +this.academicPerformance
-      .reduce((sum, mark) => (sum + mark) / this.academicPerformance.length)
-      .toFixed(2)
-  }
-  private static sortStudents(s1: IStudent, s2: IStudent) {
-    if (s1.averageMark > s2.averageMark) {
-      return 1
-    } else if (s1.averageMark < s2.averageMark) {
-      return -1
-    } else {
-      return 0
-    }
-  }
-  static sort(arr: IStudent[]) {
-    return [...arr].sort(this.sortStudents)
-  }
-  private static isMarksNineOrTen(arr: number[]) {
-    return arr.every(m => m === 9 || m === 10)
-  }
-  private static filterStudents(arr: IStudent[]) {
-    let res: IStudent[] = []
-    arr.forEach(st => {
-      if (this.isMarksNineOrTen(st.academicPerformance)) {
-        res.push(st)
-      }
-    })
-    return res
-  }
-  static printGoodStudents(arr: IStudent[]) {
-    this.filterStudents(arr).forEach(st => {
-      console.log(`Student - ${st.name}, Group - ${st.group}`)
-    })
-  }
-}
-
-let students: IStudent[] = []
-students.push(
-  new Student('Andrey', 'Semenyuk', 1, [10, 10, 10, 10, 10]),
-  new Student('Ihar', 'Luschchyk', 2, [9, 10, 9, 10, 9]),
-  new Student('Vika', 'Kushnerevich', 3, [8, 10, 7, 10, 9]),
-  new Student('Evgen', 'Ivanuyk', 3, [8, 7, 9, 8, 8]),
-  new Student('Ilya', 'Kim', 2, [4, 5, 7, 8, 9]),
-  new Student('Egor', 'Krid', 2, [3, 3, 4, 4, 5]),
-  new Student('Vasya', 'Vakulenko', 3, [3, 4, 8, 8, 7]),
-  new Student('Sergey', 'Shnurov', 1, [2, 1, 3, 3, 2]),
-  new Student('Oleg', 'Mayami', 2, [1, 2, 1, 2, 3]),
-  new Student('Viktor', 'Drobysh', 1, [2, 3, 5, 3, 8])
-)
-
-console.log(students)
-console.log(Student.sort(students))
-Student.printGoodStudents(students)
-
 // Создать массив из десяти элементов такого типа, упорядочить записи по возрастанию среднего балла.
 // Добавить возможность вывода фамилий и номеров групп студентов, имеющих оценки, равные только 4 или 5.
+
+// interface IStudent {
+//   name: string
+//   lastName: string
+//   group: number
+//   academicPerformance: number[]
+//   averageMark: number
+// }
+
+// class Student implements IStudent {
+//   name: string
+//   lastName: string
+//   group: number
+//   academicPerformance: number[]
+//   averageMark: number
+
+//   constructor(
+//     name: string,
+//     lastName: string,
+//     group: number,
+//     academicPerformance: number[]
+//   ) {
+//     this.name = name
+//     this.lastName = lastName
+//     this.group = group
+//     this.academicPerformance = academicPerformance
+//     this.averageMark = +this.academicPerformance
+//       .reduce((sum, mark) => (sum + mark) / this.academicPerformance.length)
+//       .toFixed(2)
+//   }
+//   private static sortStudents(s1: IStudent, s2: IStudent) {
+//     if (s1.averageMark > s2.averageMark) {
+//       return 1
+//     } else if (s1.averageMark < s2.averageMark) {
+//       return -1
+//     } else {
+//       return 0
+//     }
+//   }
+//   static sort(arr: IStudent[]) {
+//     return [...arr].sort(this.sortStudents)
+//   }
+//   private static isMarksNineOrTen(arr: number[]) {
+//     return arr.every(m => m === 9 || m === 10)
+//   }
+//   private static filterStudents(arr: IStudent[]) {
+//     let res: IStudent[] = []
+//     arr.forEach(st => {
+//       if (this.isMarksNineOrTen(st.academicPerformance)) {
+//         res.push(st)
+//       }
+//     })
+//     return res
+//   }
+//   static printGoodStudents(arr: IStudent[]) {
+
+//     this.filterStudents(arr).forEach(st => {
+//       console.log(`Student - ${st.name}, Group - ${st.group}`)
+//     })
+//   }
+// }
+
+// let students: IStudent[] = []
+// students.push(
+//   new Student('Andrey', 'Semenyuk', 1, [10, 10, 10, 10, 10]),
+//   new Student('Ihar', 'Luschchyk', 2, [9, 10, 9, 10, 9]),
+//   new Student('Vika', 'Kushnerevich', 3, [8, 10, 7, 10, 9]),
+//   new Student('Evgen', 'Ivanuyk', 3, [8, 7, 9, 8, 8]),
+//   new Student('Ilya', 'Kim', 2, [4, 5, 7, 8, 9]),
+//   new Student('Egor', 'Krid', 2, [3, 3, 4, 4, 5]),
+//   new Student('Vasya', 'Vakulenko', 3, [3, 4, 8, 8, 7]),
+//   new Student('Sergey', 'Shnurov', 1, [2, 1, 3, 3, 2]),
+//   new Student('Oleg', 'Mayami', 2, [1, 2, 1, 2, 3]),
+//   new Student('Viktor', 'Drobysh', 1, [2, 3, 5, 3, 8])
+// )
+
+// console.log(students)
+// console.log(Student.sort(students))
+// Student.printGoodStudents(students)
 
 // Task 02
 // Создать класс с двумя переменными. Добавить конструктор с входными параметрами и инициализирующий члены класса по умолчанию.
 // Можно ли создать метод на экземпляре класса который будет удалять сам экземпляр класса?
 // Можно ли создать метод класса который будет удалять экземпляр класса?
+
+class Test {
+  a: any
+  b: any
+  constructor(a = 1, b = 0) {
+    this.a = a
+    this.b = b
+  }
+}
+class Point {
+  x = 10
+  y = 5
+}
+console.log(new Test(3, 2))
 
 // Task 03
 // Составить описание класса для представления времени. Предусмотреть возможности установки времени и изменения его отдельных
