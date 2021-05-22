a0c7208ae6853ba8f86f1fc632b9527e0e6ba061
MrBo88 <Mr@Bo88.io>
2021-05-22 09:37:45 +0300

    lesson 7 / task 1

--- src/lessons/lesson7/lesson7.ts
+++ src/lessons/lesson7/lesson7.ts
@@ -1,4 +1,4 @@
-console.log('Lesson 7');
+console.log('Lesson 7')
 
 // __Proto__
 // https://learn.javascript.ru/prototype-inheritance
@@ -9,10 +9,8 @@ console.log('Lesson 7');
 // https://learn.javascript.ru/function-prototype
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Object/prototype
 
-
 // https://www.youtube.com/watch?v=aQkgUUmUJy4&t=21s
 // https://www.youtube.com/watch?v=b55hiUlhAzI
-
 
 //Task 01
 // Реализовать класс Animal который принимает name(по умолчанию 'Animal') в качестве параметра, у которого будет 3
@@ -20,26 +18,42 @@ console.log('Lesson 7');
 // walk => `${this.name} walking`
 // проверить, что методы работают
 
+class Animal {
+  name: string
+  constructor(name = 'Animal') {
+    this.name = name
+  }
+  walk() {
+    console.log(`${this.name} walking`)
+  }
+  eat() {
+    console.log(`${this.name} eating`)
+  }
+  sleep() {
+    console.log(`${this.name} sleeping`)
+  }
+}
+
+let bear = new Animal('Bear')
+bear.eat()
+bear.walk()
+bear.sleep()
 
 //Task 02
 // Реализовать класс Monkey на базе класса Animal,  конструктор принимает name(по умолчанию 'Monkey') в качестве
 // параметра, реализовать методы roar и climb аналогично классу Animal
 // проверить, что все методы работают
 
-
 //Task 03
 // Реализовать класс Human на базе класса Monkey, конструктор принимает name(по умолчанию 'Human') в качестве
 // параметра, реализовать методы speak и think аналогично классу Animal
 // проверить, что все методы работают
 
-
 // Task 04
 // Реализовать таски 01-03 через функции конструкторы в отдельном JS файле, реализовать наследование
-
 
 // Task 05
 // Используя метод Apply реализовать свой собственный метод bind
 
-
 // just a plug
-export default () => {};
+export default () => {}
